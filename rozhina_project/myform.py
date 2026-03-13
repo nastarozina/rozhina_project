8e91425c5b0249314631304fcaa9d19afa56b86e
nastyarozhina <Sapphire_owl@outlook.com>
2026-03-13 11:59:36 +0300

    Add the user's name and the date of the user's request in a shortened format

--- rozhina_project/myform.py
+++ rozhina_project/myform.py
@@ -1,10 +1,13 @@
 from bottle import post, request
+from datetime import date
 import re
 
 @post('/home', method='post')
 def my_form():
     pattern = r'^[A-Za-z0-9]([A-Za-z0-9._-]{0,62}[A-Za-z0-9])?@[A-Za-z0-9]+([-.][A-Za-z0-9]+)*\.[A-Za-z]{2,}$'
     mail = request.forms.get('ADRESS')
+    name = request.forms.get('USERNAME')
+    today = date.today().strftime("%Y-%m-%d")
     if not re.match(pattern, mail):
         return "Error: Invalid email format"
-    return "Thanks! The answer will be sent to the mail %s" % mail
+    return "Thanks, %s! The answer will be sent to the mail %s. Access Date: %s" % (name, mail, today)
